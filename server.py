d877698ea63f15b42e67f6d2b5a9507a205ae5a4
Sin Luong <sin@finervision.com>
2025-05-07 22:34:34 +0100

    chore: update with graceful shutdown.

--- server.py
+++ server.py
@@ -6,7 +6,9 @@ import json
 from openwakeword.model import Model
 import argparse
 import time
+import signal
 
+shutdown_event = asyncio.Event()
 # Parse input arguments
 parser = argparse.ArgumentParser()
 parser.add_argument("--chunk_size", type=int, default=2048)
@@ -42,7 +44,7 @@ async def wakeword_loop():
     cooldown_seconds = 2  # Only send one detection per 2 seconds
     print("Wake word detection started. Waiting for connections...")
 
-    while True:
+    while not shutdown_event.is_set():
         audio_chunk = np.frombuffer(mic_stream.read(CHUNK, exception_on_overflow=False), dtype=np.int16)
         prediction = owwModel.predict(audio_chunk)
 
@@ -53,9 +55,10 @@ async def wakeword_loop():
             last_detection_time = current_time
             print("Wakeword detected!")
             # Send to WebSocket clients here
-            for ws in clients:
+            await notify_clients({"message": "wakeword_detected"})
+            # for ws in clients:
                 # await ws.send("wakeword_detected")
-                await notify_clients({"message": "wakeword_detected"})
+                # await notify_clients({"message": "wakeword_detected"})
 
         await asyncio.sleep(0.001)  # yield to event loop
 
@@ -66,10 +69,40 @@ async def handler(websocket):
     finally:
         clients.remove(websocket)
 
+def shutdown_handler():
+    print("\nReceived shutdown signal. Cleaning up...")
+    shutdown_event.set()
+
+signal.signal(signal.SIGINT, lambda s, f: shutdown_handler())  # CTRL+C
+signal.signal(signal.SIGTERM, lambda s, f: shutdown_handler())  # kill command
+
+
 async def main():
     server = await websockets.serve(handler, "localhost", 9091)
     print("WebSocket server running on ws://localhost:9091")
-    await asyncio.gather(server.wait_closed(), wakeword_loop())
+
+    wakeword_task = asyncio.create_task(wakeword_loop())
+    await shutdown_event.wait()  # Wait for shutdown signal
+
+    print ("Shutting down...")
+
+    wakeword_task.cancel()  # Cancel the wakeword loop task
+
+    try:
+        await wakeword_task
+
+    except asyncio.CancelledError:
+        print("Wakeword loop cancelled.")
+        pass
+    # await asyncio.gather(server.wait_closed(), wakeword_loop())
+    # close audio sources
+    mic_stream.stop_stream()
+    mic_stream.close()
+    audio.terminate()
+
+    # close websocket server
+    server.close()
+    await server.wait_closed()
 
 if __name__ == "__main__":
     asyncio.run(main())
